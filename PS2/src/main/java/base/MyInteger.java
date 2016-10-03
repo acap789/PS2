d15a266cb143d5c3d0bc92f9e9243ffeb422a342
anthonycaporelli <anthonycaporelli@wifi-roaming-128-4-223-218.host.udel.edu>
2016-10-03 18:16:58 -0400

    PS2

---
+++ PS2/src/main/java/base/MyInteger.java
@@ -0,0 +1,80 @@
+package base;
+
+public class MyInteger {
+	
+	private int iValue;
+	
+	public int getValue() {
+		return iValue;
+	}
+	
+	public MyInteger(int num) {
+		this.iValue = num;
+	}
+	
+	public boolean isEven() {
+		if (iValue%2==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isOdd() {
+		if (iValue%2!=0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isPrime() {
+		for (int factor = 2; factor < iValue / 2; factor++) {
+			if (iValue%factor==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isEven(int n) {
+		if (n%2==0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isOdd(int n) {
+		if (n%2!=0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean isPrime(int n) {
+		for (int factor = 2; factor < n/2; factor++) {
+			if (n%factor==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isEven(MyInteger iValue) {
+		return iValue.isEven();
+	}
+	
+	public static boolean isOdd(MyInteger iValue) {
+		return iValue.isOdd();
+	}
+	
+	public static boolean isPrime(MyInteger iValue) {
+		return iValue.isPrime();
+	}
+	
+	public boolean Equals(int n) {
+		return (iValue ==n);
+	}
+	
+	public boolean Equals(MyInteger n) {
+		return Equals(n.getValue());
+	}
+}
